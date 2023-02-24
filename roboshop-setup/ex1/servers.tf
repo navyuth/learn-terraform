12f8be3e24a2d37926f0db20170f518fa4b9636c
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-24 14:24:47 +0530

    In ex1 created resource files for DNS records for all server components

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "frontend-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -15,6 +23,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "cart-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -26,6 +42,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "catalogue-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -33,6 +57,14 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "mongodb-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -44,6 +76,14 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "redis-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -51,6 +91,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "user-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -62,6 +110,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "mysql-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -69,6 +125,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "shipping-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -80,6 +144,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "rabbitmq-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
@@ -87,6 +159,14 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "payment-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "dispatch" {
@@ -97,3 +177,12 @@ resource "aws_instance" "dispatch" {
     Name = "dispatch"
   }
 }
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "dispatch-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.dispatch.private_ip]
+}
+
