9f9be60763bb2df7ccde45313f283a63604cf906
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-25 21:07:06 +0530

    created 05-s3-state and main.tf

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -11,6 +11,6 @@ resource "aws_instance" "ec2" {
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0141873d0ab9c8b98"]
   tags = {
-    Name = "Terraform Test"
+    Name = "Terraform Test1"
   }
 }
