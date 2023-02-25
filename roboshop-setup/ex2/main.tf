875289fd57617fd067f5be9ee9b0d66fc9bdff46
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-25 22:08:36 +0530

    tags Names is created in main.tf to generate names for each instance

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "frontend" {
   ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0141873d0ab9c8b98"]
+  tags = {
+    Name=var.instances[count.index]
+  }
 }
 
 variable "instances" {
