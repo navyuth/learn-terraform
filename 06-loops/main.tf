5a61e607f52fe688b067383dd075dd3176b5fb6d
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-25 22:23:44 +0530

    variable d2 as null resource and count

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -4,5 +4,12 @@ variable "d1" {
 
 resource "null_resource" "nothing" {
   count = var.d1
+}
 
+variable "d2" {
+  default = [0, 1, 2, 3, 4]
+}
+
+resource "null_resource" "d2" {
+  count = length(var.d2)
 }
