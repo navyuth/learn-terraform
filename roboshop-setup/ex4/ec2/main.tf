64f477df98d096e7dd38169cdc1c5ad0cc1eb4fd
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-27 13:15:57 +0530

    iterating the module in main.tf, created variables for component and instance_type inside ec2/main.tf

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = [973714476881]
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-0141873d0ab9c8b98"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z069905439T4JHL9YIZCX"
+  name    = "${var.component}-dev.devopscourse.online.com"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
