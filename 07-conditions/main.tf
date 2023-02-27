982815779fac0f464b8606483639f6a475647932
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-27 12:51:06 +0530

    created demo resource to check the condition for boolean values

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -8,6 +8,7 @@ variable "instance_type" {}
 variable "create_instance" {}
 
 resource "aws_instance" "demo" {
+  # if given true instance will be created, if given false in CLI then no instance will be created
   count = var.create_instance == "true" ? 1 : 0
   ami = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
