20bf2ded0c06d5263c6e37c254f5d659ff17884d
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 12:58:32 +0530

    created directory ex5 and imported sg and ec2 files and declared variables separately in vars.tf file

--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -6,6 +6,6 @@ module "ec2" {
   source = "./ec2"
   for_each = var.instances
   component = each.value["name"]
-  instance_type = each.value['type']
+  instance_type = each.value["type"]
   sg_id = module.sg.sg_id
 }
