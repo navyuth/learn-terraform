31d9619e9cc57f918b408e5d0fce57bc70cee571
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 13:22:06 +0530

    09-functions

---
+++ 09-functions/main.tf
@@ -0,0 +1,14 @@
+variable "test" {
+  default = {
+    "catalogue" = {
+      private_ip = "172.31.15.55"
+    }
+     "user"      = {
+       private_ip = "172.31.2.59"
+     }
+  }
+}
+
+output "test" {
+  value = var.test
+}
--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -12,6 +12,7 @@ module "ec2" {
 
 module "route53" {
   for_each = var.instances
+  component= each.value["name"]
   source = "./route53"
   private_ip = module.ec2.private_ip
 }
