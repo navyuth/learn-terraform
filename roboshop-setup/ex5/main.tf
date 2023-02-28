dd81cc37ad01995a4c86b3b8ae5b7b638b805759
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-28 13:27:06 +0530

    changes in ex5\main.tf

--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -14,7 +14,7 @@ module "route53" {
   for_each = var.instances
   component= each.value["name"]
   source = "./route53"
-  private_ip = module.ec2.private_ip["catalogue"]
+  private_ip = module.ec2["catalogue"].private_ip
 }
 
 output "ec2" {
