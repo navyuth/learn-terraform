db3ee772360088d4576cb63f077d32ac5a788a8f
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-09 22:03:50 +0530

    New job_name test

--- promethues-test-servers/main.tf
+++ promethues-test-servers/main.tf
@@ -19,6 +19,7 @@ resource "aws_spot_instance_request" "node2" {
   ami = "ami-01f00f562868293a1"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0141873d0ab9c8b98"]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "prom-test-node"
